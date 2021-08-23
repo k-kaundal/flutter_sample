bf0496b3668d903763cf1b7658cbecc6da208581
Sumit <sumit.kumar@slinfy.com>
2021-08-23 12:52:22 +0530

    sample app

---
+++ lib/src/ui/my_home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
