2cb254318309550a2e57b5e85f5bde52b81ca1ef
Sumit <sumit.kumar@slinfy.com>
2021-08-24 10:52:50 +0530

    sample app

--- lib/src/ui/auth/register.dart
+++ lib/src/ui/auth/register.dart
@@ -1,16 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sample/src/ui/auth/register.dart';
-import 'package:flutter_sample/src/ui/my_home.dart';
+import 'package:flutter_sample/src/ui/auth/login.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+import '../my_home.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
 
   @override
-  _LoginState createState() => _LoginState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
+class _RegisterState extends State<Register>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Size size = MediaQuery.of(context).size;
   OutlineInputBorder border = OutlineInputBorder(
@@ -20,7 +21,6 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-
     _controller = AnimationController(vsync: this);
   }
 
@@ -41,12 +41,33 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
             child: Column(
               children: [
                 Image.network(
-                  'https://alexwebdevelop.com/wp-content/uploads/2019/08/php-login-and-authentication-the-definitive-guide.webp',
+                  'https://www.drupal.org/files/styles/grid-3-2x/public/project-images/reg_confirm_email_with_button_0.png?',
                   width: size.width * 0.6,
                   height: size.height * 0.3,
                 ),
                 Padding(
                   padding: EdgeInsets.only(top: 20, bottom: 3),
+                  child: Card(
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    shadowColor: Colors.black54,
+                    child: SizedBox(
+                      height: size.height * 0.05,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            border: border,
+                            hintText: 'Enter Name',
+                            labelText: 'Name',
+                            labelStyle: TextStyle(
+                              color: Colors.blue,
+                            )),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 3, bottom: 3),
                   child: Card(
                     elevation: 3,
                     shape: RoundedRectangleBorder(
@@ -87,27 +108,18 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.all(3.0),
-                  child: SizedBox(
-                    width: size.width * 0.8,
-                    child: Text('Forgot Password?',
-                        style: TextStyle(color: Colors.blue),
-                        textAlign: TextAlign.end),
-                  ),
-                ),
-                Padding(
                   padding: EdgeInsets.only(top: 20),
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                         elevation: 5,
                         padding: EdgeInsets.only(
-                            left: size.width * 0.34, right: size.width * 0.34)),
+                            left: size.width * 0.32, right: size.width * 0.32)),
                     onPressed: () {
                       Navigator.of(context).push(
                           MaterialPageRoute(builder: (context) => MyHome()));
                     },
                     child: Text(
-                      'Login',
+                      'Register',
                     ),
                   ),
                 ),
@@ -116,16 +128,18 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Dont't have an account? "),
+                      Text("Already have an account? "),
                       InkWell(
                         onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => Register()));
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => Login()));
                         },
                         child: Text(
-                          'Sign Up',
+                          'Login',
                           style: TextStyle(
-                              color: Colors.blue, fontStyle: FontStyle.italic),
+                            color: Colors.blue,
+                            fontStyle: FontStyle.italic,
+                          ),
                         ),
                       )
                     ],
